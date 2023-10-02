e99cad6b322943f4e0dad2c1b3ba2db61555c83c
teegash <nategadgets@gmail.com>
2023-10-02 18:01:22 +0300

    first commit

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+//next image
+import Image from "next/image";
+
+// next link
+import Link from "next/link";
+
+// components
+import Socials from "../components/Socials";
+
+
+const Header = () => {
+  return <header className='absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'>
+    <div className='container mx-auto '>header</div>
+  </header>;
+};
+
+export default Header;
